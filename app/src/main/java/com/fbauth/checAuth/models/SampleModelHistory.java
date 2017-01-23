6504e1f1f36a98a338236ef901319e14108e2db3
Battula, Lakshmana Rao (US - Bengaluru) <lakshmanraob@gmail.com>
2017-01-23 15:51:26 +0530

    SampleModelHistory update

---
+++ app/src/main/java/com/fbauth/checAuth/models/SampleModelHistory.java
@@ -0,0 +1,17 @@
+package com.fbauth.checAuth.models;
+
+/**
+ * Created by labattula on 20/01/17.
+ */
+
+public class SampleModelHistory {
+    String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ app/src/main/java/com/fbauth/checAuth/models/SampleModelList.java
@@ -0,0 +1,20 @@
+package com.fbauth.checAuth.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by labattula on 20/01/17.
+ */
+
+public class SampleModelList {
+
+    ArrayList<SampleModel> sampleList;
+
+    public ArrayList<SampleModel> getSampleModelArrayList() {
+        return sampleList;
+    }
+
+    public void setSampleModelArrayList(ArrayList<SampleModel> sampleModelArrayList) {
+        this.sampleList = sampleModelArrayList;
+    }
+}

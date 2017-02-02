96f41e5eaa5300508327437c35faf8e3f4aa73e7
Battula, Lakshmana Rao (US - Bengaluru) <lakshmanraob@gmail.com>
2017-02-02 11:01:35 +0530

    Updated the launchActivity Sample Model update User Scanner screen and details update

--- app/src/main/java/com/fbauth/checAuth/models/SampleModel.java
+++ app/src/main/java/com/fbauth/checAuth/models/SampleModel.java
@@ -1,12 +1,15 @@
 package com.fbauth.checAuth.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * Created by labattula on 19/01/17.
  */
 
-public class SampleModel {
+public class SampleModel implements Parcelable {
     private String modelString;
     private String modelImage;
     private String modelDate;
@@ -19,6 +22,45 @@ public class SampleModel {
         BLOCKED,
         ENAGAGED
     }
+
+    public SampleModel(String mString, String mImage, String mDate, String mState) {
+        this.modelString = mString;
+        this.modelImage = mImage;
+        this.modelDate = mDate;
+        this.modelState = mState;
+    }
+
+    protected SampleModel(Parcel in) {
+        modelString = in.readString();
+        modelImage = in.readString();
+        modelDate = in.readString();
+        modelState = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(modelString);
+        dest.writeString(modelImage);
+        dest.writeString(modelDate);
+        dest.writeString(modelState);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<SampleModel> CREATOR = new Creator<SampleModel>() {
+        @Override
+        public SampleModel createFromParcel(Parcel in) {
+            return new SampleModel(in);
+        }
+
+        @Override
+        public SampleModel[] newArray(int size) {
+            return new SampleModel[size];
+        }
+    };
 
     public String getModelString() {
         return modelString;
@@ -58,5 +100,36 @@ public class SampleModel {
 
     public void setModelHistory(ArrayList<SampleModelHistory> modelHistory) {
         this.modelHistory = modelHistory;
+    }
+
+    public static class Builder {
+        private String modelString;
+        private String modelImage;
+        private String modelDate;
+        private String modelState;
+
+        public Builder setModelString(String str) {
+            this.modelString = str;
+            return this;
+        }
+
+        public Builder setModelImage(String str) {
+            this.modelImage = str;
+            return this;
+        }
+
+        public Builder setModelDate(String str) {
+            this.modelDate = str;
+            return this;
+        }
+
+        public Builder setModelState(String str) {
+            this.modelState = str;
+            return this;
+        }
+
+        public SampleModel build() {
+            return new SampleModel(modelString, modelImage, modelDate, modelState);
+        }
     }
 }
